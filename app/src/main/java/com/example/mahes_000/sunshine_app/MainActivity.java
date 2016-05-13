9f40b618b628c00e57a9f31fe81601b9cb534218
maheshbabugorantla <mgorantl@purdue.edu>
2016-05-12 20:57:47 -0400

    added settings_preferences.xml file to set the App Preferences

--- app/src/main/java/com/example/mahes_000/sunshine_app/MainActivity.java
+++ app/src/main/java/com/example/mahes_000/sunshine_app/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends Activity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
         {
             startActivity(new Intent(this,SettingsActivity.class));
